f680e7bd90ab78abb5e0bcd46c6411ef6f323c4a
pradeeps9 <46450532+pradeeps9@users.noreply.github.com>
2021-02-01 19:11:18 +0530

    Update StarPattern.py

--- StarPattern.py
+++ StarPattern.py
@@ -1,12 +1,6 @@
 n = int(input("Enter a number"))
 n1 = 1  # For true case
-val = input("Enter 0 for false and 1 for true")
-
-# boolval = True
-if val == "0":
-    boolval: bool = False
-elif val == "1":
-    boolval: bool = True
+boolval = bool(int(input("Enter 0 for false and 1 for true")))  # take a input then (typecast) change into integer and lastly (typecast) change into boolean 
 
 while n > 0 and n1 <= n:
     if boolval == False:
